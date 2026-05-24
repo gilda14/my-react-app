0f3a0070c96c5af9a233caa33c777245dbd492d9
Gilda <g.shirvany@yahoo.com>
2026-05-24 15:49:29 +1000

    add ES7 app in the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css'
 import { Routes, Route } from "react-router-dom"
 import FirstPage from "./pages/FirstPage"
 import HomePage from './pages/HomePage';
-
+import Secondpage from './pages/SecondPage';
 function App() {
   return (
     <>
       <Routes>
         <Route path="/first-page" element={<FirstPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path='/second-page' element={<Secondpage/>}/>
       </Routes>
+   
     </>
   );
 }
---
+++ src/pages/SecondPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <h1>Hi everyone </h1>
+   </header>
+  )
+}
+
+export default Header
