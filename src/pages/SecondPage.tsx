ea17892654cfb8d44bd004579e11b1d6edeae868
Gilda <your-email@example.com>
2026-06-02 12:02:19 +1000

    add Delete button to the shopping list

--- src/pages/SecondPage.tsx
+++ src/pages/SecondPage.tsx
@@ -13,7 +13,7 @@ type Product = {
 export default function Secondpage() {
   const navigate = useNavigate();
   const [search, setSearch] = useState("");
-  const [products, setProducts] = useState<Product[]>([
+  const [products] = useState<Product[]>([
     {
       id: 1,
       name: "Apple",
@@ -34,46 +34,50 @@ export default function Secondpage() {
     },
   ]);
   //new  shopping list
-  const [newName, setNewName] = useState("");
-  const [newPrice, setNewPrice] = useState("");
-  const [newPicture, setNewPicture] = useState("");
+  // const [newName, setNewName] = useState("");
+  // const [newPrice, setNewPrice] = useState("");
+  // const [newPicture, setNewPicture] = useState("");
   const [shoppingList, setShoppingList] = useState<Product[]>([]);
 
   function handleOnClick() {
     navigate("/first-page");
   }
 
-  function handleAddProduct() {
-    if (
-      newName.trim() === "" ||
-      newPrice.trim() === "" ||
-      newPicture.trim() === ""
-    )
-      return;
+  // function handleAddProduct() {
+  //   if (
+  //     newName.trim() === "" ||
+  //     newPrice.trim() === "" ||
+  //     newPicture.trim() === ""
+  //   )
+  //     return;
 
-    const newProduct: Product = {
-      id: products.length ? products[products.length - 1].id + 1 : 1,
-      name: newName,
-      picture: newPicture,
-      price: Number(newPrice),
-    };
+  //   const newProduct: Product = {
+  //     id: products.length ? products[products.length - 1].id + 1 : 1,
+  //     name: newName,
+  //     picture: newPicture,
+  //     price: Number(newPrice),
+  //   };
 
-    setProducts([...products, newProduct]);
+  //   setProducts([...products, newProduct]);
 
-    setNewName("");
-    setNewPrice("");
-    setNewPicture("");
-  }
+  //   setNewName("");
+  //   setNewPrice("");
+  //   setNewPicture("");
+  // }
 
   //Add Item to the shopping list
-  function handAddToList(product: product) {
+  function handAddToList(product: Product) {
     setShoppingList([...shoppingList, product]);
   }
 
+  //delete item from shopping list
+  function handleDeleteItem(id: number) {
+    setShoppingList(shoppingList.filter((product) => product.id !== id));
+  }
   return (
     <PageTemplate>
       <div className="div">
-        <h3>Shopping list</h3>
+        <h3>All Products</h3>
 
         <SearchItem setSearch={setSearch} />
 
@@ -94,8 +98,6 @@ export default function Secondpage() {
               </div>
             ))}
         </div>
-
-        <h4>New Shopping List </h4>
 
         {/* <input
           type="text"
@@ -119,15 +121,19 @@ export default function Secondpage() {
         />
 
         <button onClick={handleAddProduct}>Add Product</button> */}
+
+        <h4>New Shopping List </h4>
         <div>
           {shoppingList.length === 0 ? (
-            <p>No items added yet.</p>
+            <p>You dont have anything in your shopping list yet</p>
           ) : (
-            shoppingList.map((product, index) => (
-              <div className="item" key={`${product.id}-${index}`}>
+            shoppingList.map((product) => (
+              <div className="item" key={product.id}>
                 <h4>{product.name}</h4>
-
                 <p>${product.price}</p>
+                <button onClick={() => handleDeleteItem(product.id)}>
+                  Delete
+                </button>
               </div>
             ))
           )}
